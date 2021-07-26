9b956df56ef18f3c1415b6ef178a7ae4319a5c2f
Omkar Sawant <omkarsawant1507@gmail.com>
2021-07-26 09:20:23 +0530

    Connected components and problem related to it.

---
+++ Graphs/selectTwoFriendsFromDifferentFriendCircle.cpp
@@ -0,0 +1,52 @@
+//Find the number ways in which you can select two friends each from a different friend circle.
+//Given three friend circle with sizes 2,3,4.
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int getSize(int idx, vector<vector<int>> &adj, vector<bool> &visited){
+    if(visited[idx]){ //Not for first call but for inner calls
+        return 0;
+    }  
+    visited[idx] = true;
+    int ans = 1;
+    for(auto it : adj[idx]){
+        if(!visited[it]){
+            ans += getSize(it,adj,visited);
+        }
+    }
+    return ans;
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+
+    vector<vector<int>> adj(n);
+    vector<bool> visited(n,false);
+    vector<int> components;
+
+    int u,v;
+    for(int i=0; i<m; i++){
+        cin>>u>>v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    for(int i=0; i<n; i++){
+        if(!visited[i]){
+            components.push_back(getSize(i,adj,visited));
+        }
+    }
+
+    int ways = 0;
+    for(auto element : components){
+        ways += element * (n-element); 
+    }
+
+    cout<<"Number of ways: "<<(ways/2);
+
+    return 0;
+}
