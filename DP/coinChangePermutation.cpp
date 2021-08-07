e154824c5a81e3b6eb9a2b7f7db6c869105119fc
Omkar Sawant <omkarsawant1507@gmail.com>
2021-08-07 18:39:50 +0530

    Changed name in permutation problem for better understanding.

--- DP/coinChangePermutation.cpp
+++ DP/coinChangePermutation.cpp
@@ -6,10 +6,10 @@ int main(){
     int n;
     cin>>n;
 
-    int arr[n];
+    int coins[n];
 
     for(int i=0; i<n; i++){
-        cin>>arr[i];
+        cin>>coins[i];
     }
 
     int amount;
@@ -21,8 +21,8 @@ int main(){
 
     for(int i=1; i<amount+1; i++){
         for(int j=0; j<n; j++){//Selecting all coins for each amount
-            if(arr[j]<=i){
-                dp[i] += dp[i-arr[j]];
+            if(coins[j]<=i){
+                dp[i] += dp[i-coins[j]];
             }
         }
     }
