b904e123d346f1266e11fba841e26f118c2bb864
Omkar Sawant <omkarsawant1507@gmail.com>
2021-11-18 12:17:19 +0530

    by both tabulation and memoization

--- DPL2/maxAlternatingSubsequenceSum.cpp
+++ DPL2/maxAlternatingSubsequenceSum.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
+
+int memo(int idx, int *arr, int p, vector<vector<int>> &dp, int n){
+
+    if(idx>=n) return 0;
+
+    if(dp[idx][p] != -1){
+        return dp[idx][p];
+    }
+
+    int ans;
+
+    //choose element
+    if(p==0){
+        ans = memo(idx+1, arr, 1, dp, n) + arr[idx];
+    }else{
+        ans = memo(idx+1, arr, 0, dp, n) - arr[idx];
+    }
+
+    return dp[idx][p] = max(ans, memo(idx+1, arr, p, dp, n));
+
+}
+
+int tab(int *arr, int n){
+    int endEven = arr[0];
+    int endOdd = 0;
+
+    for(int i=1; i<n; i++){
+        endEven = max(endEven, endOdd + arr[i]);
+        endOdd = max(endOdd, endEven - arr[i]);
+    }
+
+    return endEven;
+}
 
 int main(){
     int n;
@@ -12,15 +46,10 @@ int main(){
         cin>>arr[i];
     }
 
-    int endEven = arr[0];
-    int endOdd = 0;
+    cout<<tab(arr, n)<<"\n";
 
-    for(int i=1; i<n; i++){
-        endEven = max(endEven, endOdd + arr[i]);
-        endOdd = max(endOdd, endEven - arr[i]);
-    }
-
-    cout<<endEven;
+    vector<vector<int>> dp(n, vector<int>(2,-1));
+    cout<<memo(0, arr, 0, dp, n);
 
     return 0;
 }
